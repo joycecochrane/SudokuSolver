6864cb6a9546077fad95e408a0ba16c43de20475
Joyce <joyce.ngu@gmail.com>
2016-10-07 13:19:00 -0700

    first commit

---
+++ sudoku/sudoku/SudokuSolver.java
@@ -0,0 +1,27 @@
+//package sudoku;
+
+/**
+ * Place for your code.
+ */
+public class SudokuSolver {
+
+	/**
+	 * @return names of the authors and their student IDs (1 per line).
+	 */
+	public String authors() {
+		return "Author 1: Joyce Ngu - 16179146\n" +
+				"Author 2: Alice Foster - 41250145";
+	}
+
+	/**
+	 * Performs constraint satisfaction on the given Sudoku board using Arc Consistency
+	 * and Domain Splitting.
+	 * 
+	 * @param board the 2d int array representing the Sudoku board. Zeros indicate unfilled cells.
+	 * @return the solved Sudoku board
+	 */
+	public int[][] solve(int[][] board) {
+
+		return board;
+	}
+}
